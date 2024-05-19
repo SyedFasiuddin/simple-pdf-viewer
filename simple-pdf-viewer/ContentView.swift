f7cffde27148133cc545145da420ad6f867316ac
Syed Fasiuddin <syedfasiuddin.02@gmail.com>
2024-05-19 22:11:49 +0530

    Initial Commit

---
+++ simple-pdf-viewer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  simple-pdf-viewer
+//
+//  Created by Syed Fasiuddin on 5/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ simple-pdf-viewer/simple_pdf_viewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  simple_pdf_viewerApp.swift
+//  simple-pdf-viewer
+//
+//  Created by Syed Fasiuddin on 5/19/24.
+//
+
+import SwiftUI
+
+@main
+struct simple_pdf_viewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
