d14a3b96be4842172533b79a911168fcf9ac0489
Syed Fasiuddin <syedfasiuddin.02@gmail.com>
2024-05-20 11:45:24 +0530

    all functionality

--- simple-pdf-viewer/ContentView.swift
+++ simple-pdf-viewer/ContentView.swift
@@ -6,17 +6,49 @@
 //
 
 import SwiftUI
-
+import PDFKit
+import UniformTypeIdentifiers
+    
 struct ContentView: View {
+    @State var filename = ""
+    @State var selectFileURL: URL?
+    @State var previewURL: URL?
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            if let url = selectFileURL {
+                PDFKitView(url: url)
+            } else {
+                Button("Select File") {
+                    let panel = NSOpenPanel()
+                    panel.allowsMultipleSelection = false
+                    panel.canChooseDirectories = false
+                    panel.allowedContentTypes = [UTType.pdf].compactMap { $0 }
+                    if panel.runModal() == .OK {
+                        self.selectFileURL = panel.url
+                        self.filename = panel.url?.lastPathComponent ?? "<none>"
+                    }
+                }
+                .hidden()
+                .keyboardShortcut("o", modifiers: [.command])
+            }
         }
-        .padding()
     }
+}
+
+struct PDFKitView: NSViewRepresentable {
+    var url : URL
+    func makeNSView(context: Context) -> PDFView {
+        let pdfView = PDFView()
+        
+        pdfView.document = PDFDocument(url:url)
+        pdfView.displaysPageBreaks = false
+        pdfView.pageShadowsEnabled = false
+        
+        return pdfView
+    }
+    
+    func updateNSView(_ nsView: PDFView, context: Context) {  }
 }
 
 #Preview {
